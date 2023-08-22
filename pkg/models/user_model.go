198aabb878b4d13956ff1bedbf79c71d7bc4b512
PaulDieterich <Paul.dieterich0@gmail.com>
2023-08-22 18:56:18 +0200

    add Role struct

--- pkg/models/user_model.go
+++ pkg/models/user_model.go
@@ -1,8 +1,13 @@
 package models
 
+type Role struct {
+	Name string
+}
+
 type User struct {
 	Id       int    `gorm:"primary key; autoIncrement" json:"uuid"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+	Role     Role   `json:"role"`
 }
